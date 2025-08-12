c9bf7c07f268c1c2fb43620a4f1fc9bf9307afa8
Ahmed A <163431268+ahmed-almouna@users.noreply.github.com>
2025-08-12 19:29:29 -0400

    Update favicon and apple-touch-icon references

--- PersonalWebsite-main/src/app/layout.tsx
+++ PersonalWebsite-main/src/app/layout.tsx
@@ -20,22 +20,16 @@ export default function RootLayout({
   return (
     <html lang="en" className="dotted">
       <Head>
-        <meta {...metadata}></meta>
         <title>{metadata.title}</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" href="/favicon.ico"/>
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png"/>
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png"/>
+
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content={metadata["aria-description"]} />
         <meta name="keywords" content="Ahmed Almouna, Portfolio, Software Engineer, Web Developer" />
         <meta name="author" content="Ahmed Almouna" />
-
-        <link rel="icon" href="/favicon.ico" sizes="210x210" type="image/x-icon"/>
-        <link rel="icon" href="/favicon.png" sizes="210x210" type="image/png"/>
-        <link rel="shortcut icon" href="/favicon.ico" sizes="210x210" type="image/x-icon" />
-        <link rel="shortcut icon" href="/favicon.png" sizes="210x210" type="image/png" />
-
-        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png"/>
-        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png"/>
-
-        <link rel="apple-touch-icon" href="/favicon-180x180.png" sizes="180x180" type="image/png" />
       </Head>
       <body className={poppins.className}>{children}</body>
     </html>
